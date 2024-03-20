3163dc9dc706925ac92f81a2716484cb09368d2d
ValentinAmazing <podrezva@bk.ru>
2024-03-21 01:09:12 +0700

    del enum Alert

--- L3.02 hw Networking/ViewControllers/MainViewController.swift
+++ L3.02 hw Networking/ViewControllers/MainViewController.swift
@@ -1,28 +1,6 @@
 
 import UIKit
 
-enum Alert {
-    case success
-    case failed
-    
-    var title: String {
-        switch self {
-        case .success:
-            return "Success"
-        case .failed:
-            return "Failed"
-        }
-    }
-    
-    var message: String {
-        switch self {
-        case .success:
-            return "You can see the results in the Debug area"
-        case .failed:
-            return "You can see error in the Debug area"
-        }
-    }
-}
 
 final class MainViewController: UITableViewController {
 
