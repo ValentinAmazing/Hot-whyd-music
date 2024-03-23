120a676cc4bac9bdb1c56403a1c8afa8404e12de
ValentinAmazing <podrezva@bk.ru>
2024-03-23 19:38:05 +0700

    оставил только нужные свойства в модели данных JSON

--- Hot whyd music/Models/trackList.swift
+++ Hot whyd music/Models/trackList.swift
@@ -2,32 +2,13 @@
 import Foundation
 
 
-///
 struct TrackList: Decodable {
-    let hasMore: HasMore
     let tracks: [Track]
 }
-///
-struct HasMore: Decodable {
-    let skip: Int
-}
-///
+
 struct Track: Decodable {
-    let _id: String
-    let eId: String
     let name: String
     let img: URL?
-    let uId: String
-    let uNm: String
-    let pl: PlayList?
-    let pId: String
-    let nbR, nbL: Int
-    let score: Int
     let trackUrl: String
-}
-///
-struct PlayList: Decodable {
-    let id: Int
-    let name: String
 }
 
